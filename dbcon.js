81a93de9c33c7b8aa9eb5177b9bb9154c27d8482
Kevin J Graebel <kgraebel@gmail.com>
2019-11-28 13:14:43 -0600

    Working database link

--- dbcon.js
+++ dbcon.js
@@ -2,7 +2,7 @@ var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
   host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'graebelk',
+  user            : 'cs361_graebelk',
   password        : '9859',
   database        : 'cs361_graebelk'
 });
